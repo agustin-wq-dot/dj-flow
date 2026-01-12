5a95a47c2a3500faacfb3f35bd5387022782d624
agustin-wq-dot <agustin.delgado@gmail.com>
2026-01-12 12:07:25 -0300

    Refactor App component with PlayerProvider and AutoPlayer

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import { PlayerProvider } from "@/context/PlayerContext";
+import { useAutoPlayer } from "@/hooks/useAutoPlayer";
+import Home from "@/pages/Home";
+import Manual from "@/pages/Manual";
 
-const queryClient = new QueryClient();
+function AutoPlayerMount() {
+  useAutoPlayer();
+  return null;
+}
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
+export default function App() {
+  return (
+    <PlayerProvider>
+      <AutoPlayerMount />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/manual" element={<Manual />} />
         </Routes>
       </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+    </PlayerProvider>
+  );
+}
