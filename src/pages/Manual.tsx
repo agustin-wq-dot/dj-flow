b6f44280713d36901ce78b35a3987b014179e928
agustin-wq-dot <agustin.delgado@gmail.com>
2026-01-12 13:46:10 -0300

    Refactor Manual component to use default Decks
    Simplified Manual component by removing props from Decks.

--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -1,21 +1,9 @@
-import { Deck as Decks } from "@/components/dj/Deck";
-
+import Decks from "@/components/dj/Deck";
 export default function Manual() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h2>Modo Manual</h2>
-      <Decks 
-        deckId="A" 
-        containerId="deck-a" 
-        state={{ isPlaying: false, track: null, playbackRate: 1 }} 
-        effectiveVolume={50}
-        onPlay={() => {}}
-        onPause={() => {}}
-        onSeek={() => {}}
-        onVolumeChange={() => {}}
-        onPlaybackRateChange={() => {}}
-        onToggleLoop={() => {}}
-      />
-    </div>
-  );
-}
+ return (
+ <div style={{ padding: 24 }}>
+ <h2>Modo Manual</h2>
+ <Decks />
+ </div>
+ );
+ }
