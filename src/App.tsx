104cd827a4019fe3c111223f613834faa976eded
agustin-wq-dot <agustin.delgado@gmail.com>
2026-01-12 12:23:06 -0300

    Refactor App component and remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PlayerProvider } from "@/context/PlayerContext";
-import { useAutoPlayer } from "@/hooks/useAutoPlayer";
-import Home from "@/pages/Home";
-import Manual from "@/pages/Manual";
+import { PlayerProvider } from '@/context/PlayerContext';
+import { ManualDJPage } from '@/pages/ManualDJPage';
 
-function AutoPlayerMount() {
-  useAutoPlayer();
-  return null;
-}
-
-export default function App() {
+function App() {
   return (
     <PlayerProvider>
-      <AutoPlayerMount />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/manual" element={<Manual />} />
-        </Routes>
-      </BrowserRouter>
+      <ManualDJPage />
     </PlayerProvider>
   );
 }
+
+export default App;
