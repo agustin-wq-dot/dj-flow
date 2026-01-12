8603105bb9daa46c8887be846c38e3f020a3ef94
agustin-wq-dot <agustin.delgado@gmail.com>
2026-01-12 12:52:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PlayerProvider } from "@/context/PlayerContext";
 import Home from "@/pages/Home";
 import Manual from "@/pages/Manual";
+import NotFound from "@/pages/NotFound";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/manual" element={<Manual />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </PlayerProvider>
