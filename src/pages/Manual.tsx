24938621b78c2cecf74020d592b3737ec8746634
agustin-wq-dot <agustin.delgado@gmail.com>
2026-01-12 13:41:26 -0300

    Enhance Manual component with Decks props

--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -1,10 +1,21 @@
-import Decks from "@/components/dj/Deck";
+import { Deck as Decks } from "@/components/dj/Deck";
 
 export default function Manual() {
   return (
     <div style={{ padding: 24 }}>
       <h2>Modo Manual</h2>
-      <Decks />
+      <Decks 
+        deckId="A" 
+        containerId="deck-a" 
+        state={{ isPlaying: false, track: null, playbackRate: 1 }} 
+        effectiveVolume={50}
+        onPlay={() => {}}
+        onPause={() => {}}
+        onSeek={() => {}}
+        onVolumeChange={() => {}}
+        onPlaybackRateChange={() => {}}
+        onToggleLoop={() => {}}
+      />
     </div>
   );
 }
