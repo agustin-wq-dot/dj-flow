dbaf5a2497aace62886f727b0f51297bfbd242db
agustin-wq-dot <agustin.delgado@gmail.com>
2026-01-12 12:49:05 -0300

    Refactor App component to use React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { PlayerProvider } from '@/context/PlayerContext';
-import { ManualDJPage } from '@/pages/ManualDJPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PlayerProvider } from "@/context/PlayerContext";
+import Home from "@/pages/Home";
+import Manual from "@/pages/Manual";
 
-function App() {
+export default function App() {
   return (
     <PlayerProvider>
-      <ManualDJPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/manual" element={<Manual />} />
+        </Routes>
+      </BrowserRouter>
     </PlayerProvider>
   );
 }
-
-export default App;
